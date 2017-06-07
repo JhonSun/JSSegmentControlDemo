de4a8c728e03057a5d4e5477ab6302bb0c425549
jhonsun <1193086345@qq.com>
2017-06-07 16:26:08 +0800

    Initial Commit

---
+++ JSSegmentControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSSegmentControlDemo
+//
+//  Created by ShengWang Gao on 2017/6/7.
+//  Copyright © 2017年 ShengWang Gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSSegmentControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSSegmentControlDemo
+//
+//  Created by ShengWang Gao on 2017/6/7.
+//  Copyright © 2017年 ShengWang Gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
