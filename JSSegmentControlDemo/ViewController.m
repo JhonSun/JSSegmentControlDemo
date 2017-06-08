a8d25a41aff6cf6863cff7e18edee747bec824e3
jhonsun <1193086345@qq.com>
2017-06-08 09:30:55 +0800

    自定义分段控制器
    有两种风格（滑条，改变背景色），滑条风格需要设置宽度，改变背景色的自适应宽度

--- JSSegmentControlDemo/ViewController.m
+++ JSSegmentControlDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "JSSegmentControl.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) JSSegmentControl *segmentControl;
 
 @end
 
@@ -16,13 +19,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:self.segmentControl];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - get
+- (JSSegmentControl *)segmentControl {
+    if (!_segmentControl) {
+        _segmentControl = [[JSSegmentControl alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 50)];
+        NSArray *titleArray = @[@"周一", @"周二", @"周三", @"周四", @"周五", @"周六", @"周日", @"周六", @"周五", @"周四", @"周三", @"周二", @"周一"];
+        _segmentControl.config.buttonWidth = 80;//[UIScreen mainScreen].bounds.size.width / titleArray.count;
+        _segmentControl.config.isAutoAdaptWidth = YES;
+        _segmentControl.config.buttonSpace = 20;
+        _segmentControl.config.buttonHeight = 30;
+        _segmentControl.config.buttonCorner = 5;
+        _segmentControl.titleArray = titleArray;
+    }
+    return _segmentControl;
 }
 
 
